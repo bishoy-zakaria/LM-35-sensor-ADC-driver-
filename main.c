71fb1fbb98847293d1d0df55cf4c1625ea4107bd
bishoy-zakaria <bishoyzakaria1999@gmail.com>
2020-07-15 15:27:08 +0200

    new update with comments in main function

--- main.c
+++ main.c
@@ -15,18 +15,23 @@
 #define F_CPU 16000000
 #include <util/delay.h>
 
-uint8  arr[4];
-uint8 counter=0;
-uint8 detect=0;
+uint8  arr[4];    //temporary array that its elements are entered from the keypad
+uint8 counter=0;  //counter that countrol the array and lcd movment
+uint8 detect=0;   //detecor that take the value of pass_check (0,1)
 
-uint16 temp=0;
-uint16 ADC_read =0;
-uint8 value = 0;
+uint16 temp=0;        // this variable is used for getting temperature value in ISR 
+uint16 ADC_read =0;   //this variable is used for getting the value of ADC_ADJUST in ISR
+ 
+uint8 value = 0;      // this variable is used for checking if the key pad is 'c' or not
 
 int main(void)
 {
+	/* ************************** INITIALIZATION ******************************************** */
+	
 	LCD_Init();
 	KeyPad_Init();
+	
+	/* **************************** USERINTERFACE ********************************************* */
 	
 	LCD_WriteString("ENTER & Press'C'");
 	LCD_GoTo( 1,6);
@@ -36,11 +41,11 @@ int main(void)
  {
 	value=KeyPad_GetValue();
 	
-	if (value !=0 && value !=67)
+	if (value !=0 && value !=67)       //entering the pass word and checking that is not equal to 'c'
 	{
-		while(KeyPad_GetValue() != 0);
+		while(KeyPad_GetValue() != 0);   //this loop is for use just on push for the putton of the keypad 
 		
-			if (counter < 3)
+			if (counter < 3)            //checking if the counter is smaller than 3 and rising its value by one 
 			{
 				arr[counter]=value;
 				LCD_GoTo( 1 , 6+counter);
@@ -48,7 +53,7 @@ int main(void)
 				counter++;
 				_delay_ms(500);
 			}
-			else if (counter == 3)
+			else if (counter == 3)   //checking if the counter is equal to 3 and getting its value to zero
 			{
 				arr[counter]=value;
 				LCD_GoTo( 1 , 9);
@@ -58,12 +63,12 @@ int main(void)
 			}
 		}
 		
-	else if (value == 67)
+	else if (value == 67)        // checking the value is entered is equal to 'c' then check the password that have entered
 	{
 		while(KeyPad_GetValue() != 0);
 		detect=pass_check(arr);
 		
-		while (detect)
+		while (detect)         // while the password is true this loop will access to all the following functions
 		{
 			LCD_Clear();
 			LCD_WriteString("Welcome Sir");
@@ -71,8 +76,7 @@ int main(void)
 			LCD_Clear();
 			ADC_INIT();
 			
-			#if ADC_INTERRUPT_DEFINE    ==    ADC_INTERRUPT_DISABLE
-			LCD_Init();
+			#if ADC_INTERRUPT_DEFINE    ==    ADC_INTERRUPT_DISABLE 
 			while (1)
 			{
 				temp=LM_35_read();
